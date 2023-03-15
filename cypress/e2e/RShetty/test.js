6185d1758f4eef840ca9753985e94a6e5b602422
chrval59279 <kramdjursbutiken@gmail.com>
2023-03-15 20:08:55 +0100

    new test

---
+++ cypress/e2e/RShetty/test.js
@@ -0,0 +1,9 @@
+import { describe } from "mocha";
+
+describe('test H1',()=>{
+
+    it('H1 is correct',()=>{
+        cy.visit('https://chrval59279.azurewebsites.net/')
+        cy.get('H1').should('contain','RahulShettyAcademy.com Learning')
+    })
+})
