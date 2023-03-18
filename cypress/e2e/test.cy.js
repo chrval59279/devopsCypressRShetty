2051c36a0622d553e9c331b6b74dad7580045914
chrval59279 <kramdjursbutiken@gmail.com>
2023-03-18 20:09:36 +0100

    h2 to h3

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,8 +1,14 @@
 
 
 describe('test',()=>{
-    it('test',()=>{
+    beforeEach(()=>{
         cy.visit('https://chrval59279.azurewebsites.net/')
+    })
+    it('test H1',()=>{
+        
         cy.get('h1').should('contain','RahulShettyAcademy.com Learning')
+    })
+    it('test H',()=>{
+        cy.get('h3').should('contain','Cypress')
     })
 })
