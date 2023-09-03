348f285fad15f178afe2d93dedd7ad6b14b11d0f
christophe <kramdjursbutiken@gmail.com>
2023-09-03 14:25:42 +0200

    update h2 and h3

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -9,9 +9,9 @@ describe('test',()=>{
         cy.get('h1').should('contain','RahulShettyAcademy.com Learning')
     })
     it('test H2',()=>{
-        cy.get('h2').should('contain','cypress')
+        cy.get('h2').should('contain','changes')
     })
     it('test H3',()=>{
-        cy.get('h3').should('contain','cypress ')
+        cy.get('h3').should('contain','cypress tests')
     })
 })
