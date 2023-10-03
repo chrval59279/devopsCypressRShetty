301a318525dc60a8822526f0e515bcd8454217c2
christophe <kramdjursbutiken@gmail.com>
2023-10-03 11:44:58 +0200

    two test for localhost and azureweb

---
+++ cypress/e2e/testazureweb.cy.js
@@ -0,0 +1,18 @@
+describe('test',()=>{
+    beforeEach(()=>{
+        cy.visit('https://chrivale.azurewebsites.net/webapp/')
+    })
+    
+    it('url',()=>{
+        cy.url().should('include','azure')
+    })
+    it('test H1',()=>{
+        cy.get('h1').should('contain','RahulShettyAcademy.com Learning')
+    })
+    it('test H2',()=>{
+        cy.get('h2').should('contain','changes')
+    })
+    it('test H3',()=>{
+        cy.get('h3').should('contain','cypress tests')
+    })
+})
--- cypress/e2e/testlocalhost.cy.js
+++ cypress/e2e/testlocalhost.cy.js
@@ -1,11 +1,14 @@
 
 
-describe('test',()=>{
+describe('test localhost',()=>{
     beforeEach(()=>{
-        cy.visit('https://chrivale.azurewebsites.net/webapp/')
+        cy.visit('localhost:8080/webapp')
+    })
+    
+    it('url',()=>{
+        cy.url().should('include','8080')
     })
     it('test H1',()=>{
-        
         cy.get('h1').should('contain','RahulShettyAcademy.com Learning')
     })
     it('test H2',()=>{
